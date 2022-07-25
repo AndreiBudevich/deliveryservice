720b061bef77b66d6e2ea7f7582665ea57383fad
Andrei Budevich <budevichac01@gmail.com>
2022-07-25 16:31:08 +0300

    add JsonUtil

---
+++ src/main/java/util/JsonUtil.java
@@ -0,0 +1,18 @@
+package util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class JsonUtil {
+
+    private static final ObjectMapper mapper = JsonMapper.builder()
+            .addModule(new JavaTimeModule())
+            .build();
+}
+
+
+
+
