ab5c131f53314cc22a9e9a33f6276d02e14f1b10
Andrei Budevich <budevichac01@gmail.com>
2022-07-25 17:33:54 +0300

    add fields Order

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,4 +1,19 @@
 package model;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true)
 public class Order extends BaseEntity {
+
+    LocalDateTime dateTime;
+    Shop shop;
+    Long totalCost;
+    String deliveryAddress;
+    List <Product> products;
 }
