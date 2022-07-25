2a2575de2a7a0d57fd3287b09037e55675f45cad
Andrei Budevich <budevichac01@gmail.com>
2022-07-25 17:26:26 +0300

    add fields Shop

--- src/main/java/model/Shop.java
+++ src/main/java/model/Shop.java
@@ -1,4 +1,17 @@
 package model;
 
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true)
 public class Shop extends NamedEntity {
+
+    private String address;
+    private String description;
+    private String contact;
+    private List <Product> products;
 }
