2c57a2e424e054ae29d2f20911f2d5e9c2b44e31
Andrei Budevich <budevichac01@gmail.com>
2022-07-25 15:54:51 +0300

    add BaseEntity

---
+++ src/main/java/model/BaseEntity.java
@@ -0,0 +1,32 @@
+package model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseEntity {
+
+    protected Integer id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !this.getClass().equals(o.getClass())) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+}
