7cb7f66deacca397cce2e736e5a93799cbf4fd05
Andrei Budevich <budevichac01@gmail.com>
2022-07-25 17:20:17 +0300

    add fields Product

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,15 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true)
 public class Product extends NamedEntity {
+
+    private String description;
+    private Shop shop;
+    private Long price;
+    private Integer discount;
 }
