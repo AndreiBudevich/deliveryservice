a4a13a6f565b15af5088a77d3dde15975bc0839a
Andrei Budevich <budevichac01@gmail.com>
2022-07-25 15:55:15 +0300

    add NamedEntity

---
+++ src/main/java/model/NamedEntity.java
@@ -0,0 +1,24 @@
+package model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class NamedEntity extends BaseEntity {
+
+    protected String name;
+
+    protected NamedEntity(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + '[' + name + ']';
+    }
+}
