d1c190d9ca9c5ba000d921dd72c8ca340d9eb842
Andrei Budevich <budevichac01@gmail.com>
2022-07-25 16:44:33 +0300

    add fields Client

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,4 +1,21 @@
 package model;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true, exclude = "orders")
 public class Client extends NamedEntity {
+
+    private String surname;
+    private String middleName;
+    private String residentialAddress;
+    private LocalDate dateOfBirth;
+    private LocalDateTime registered;
+    private List<Order> orders;
 }
