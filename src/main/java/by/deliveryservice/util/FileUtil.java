5bf92cfc8d31daed88b7c9a5d33380e8467a2905
Andrei Budevich <budevichac01@gmail.com>
2022-08-01 20:15:49 +0300

    add #isEmpty; #write

--- src/main/java/by/deliveryservice/util/FileUtil.java
+++ src/main/java/by/deliveryservice/util/FileUtil.java
@@ -3,7 +3,10 @@ package by.deliveryservice.util;
 import lombok.experimental.UtilityClass;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Objects;
 
 @UtilityClass
@@ -12,5 +15,17 @@ public class FileUtil {
     public static File getFileInResource(String nameFile) {
         String path = Objects.requireNonNull(JsonUtil.class.getResource("/")).getPath();
         return Paths.get((path + nameFile).substring(1)).toFile();
+    }
+
+    public static boolean isEmpty(String nameFile) {
+        return getFileInResource(nameFile).length() == 0;
+    }
+
+    public void write(String nameFile, String json) {
+        try {
+            Files.write(getFileInResource(nameFile).toPath(), json.getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid write file " + nameFile + " to JSON:\n", e);
+        }
     }
 }
