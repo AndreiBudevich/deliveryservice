1f997d6c4713e9e4e46d9be1e75a3cf166afe8c1
Andrei Budevich <budevichac01@gmail.com>
2022-08-01 20:41:34 +0300

    add #Category

--- src/main/java/by/deliveryservice/model/Category.java
+++ src/main/java/by/deliveryservice/model/Category.java
@@ -1,4 +1,13 @@
 package by.deliveryservice.model;
 
-public class Category extends NamedEntity{
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Category extends NamedEntity {
+    public Category(String name) {
+        super(null, name);
+    }
 }
