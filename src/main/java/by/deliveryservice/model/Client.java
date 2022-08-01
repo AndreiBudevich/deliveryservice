d47354a232d8871e7696c9236b766877d9c82e06
Andrei Budevich <budevichac01@gmail.com>
2022-08-01 20:43:18 +0300

    add @JsonProperty in Client

--- src/main/java/by/deliveryservice/model/Client.java
+++ src/main/java/by/deliveryservice/model/Client.java
@@ -1,21 +1,55 @@
 package by.deliveryservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static by.deliveryservice.util.DateTimeUtil.getCurrentDateTime;
+
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @ToString(callSuper = true, exclude = "orders")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Client extends NamedEntity {
 
+    @JsonProperty
     private String surname;
+
+    @JsonProperty
     private String middleName;
+
+    @JsonProperty
     private String residentialAddress;
+
+    @JsonProperty
     private LocalDate dateOfBirth;
+
+    @JsonProperty
     private LocalDateTime registered;
+
+    @JsonProperty
     private List<Order> orders;
+
+    public Client(String name, String surname, String middleName, String residentialAddress, LocalDate dateOfBirth) {
+        super(null, name);
+        this.surname = surname;
+        this.middleName = middleName;
+        this.residentialAddress = residentialAddress;
+        this.dateOfBirth = dateOfBirth;
+        this.registered = getCurrentDateTime();
+    }
+
+    public Client(Integer id,String name, String surname, String middleName, String residentialAddress, LocalDate dateOfBirth, LocalDateTime registered) {
+        super(id, name);
+        this.surname = surname;
+        this.middleName = middleName;
+        this.residentialAddress = residentialAddress;
+        this.dateOfBirth = dateOfBirth;
+        this.registered = registered;
+    }
 }
