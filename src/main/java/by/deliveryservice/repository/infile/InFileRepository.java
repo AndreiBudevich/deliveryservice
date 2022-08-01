08c259cfe431be40bdfb3733a3cab7f3bfe995cd
Andrei Budevich <budevichac01@gmail.com>
2022-08-01 20:16:12 +0300

    add InFileRepository

---
+++ src/main/java/by/deliveryservice/repository/infile/InFileRepository.java
@@ -0,0 +1,74 @@
+package by.deliveryservice.repository.infile;
+
+import by.deliveryservice.model.BaseEntity;
+import by.deliveryservice.repository.Repository;
+
+import java.util.*;
+
+import static by.deliveryservice.util.FileUtil.isEmpty;
+import static by.deliveryservice.util.JsonUtil.readValues;
+import static by.deliveryservice.util.JsonUtil.writeEntity;
+
+public class InFileRepository<T extends BaseEntity> implements Repository<T> {
+
+    private final String nameFile;
+    private final Class<T> clazz;
+    private Map<Integer, T> repositoryInMemory = new HashMap<>();
+
+    public InFileRepository(String nameFile, Class<T> clazz) {
+        this.nameFile = nameFile;
+        this.clazz = clazz;
+    }
+
+    @Override
+    public List<T> getAll() {
+        readInFile();
+        return new ArrayList<>(repositoryInMemory.values());
+    }
+
+    @Override
+    public Optional<T> get(int id) {
+        readInFile();
+        return Optional.of(repositoryInMemory.get(id));
+    }
+
+    @Override
+    public void delete(int id) {
+        readInFile();
+        T remove = repositoryInMemory.remove(id);
+        saveInFile();
+        System.out.println("remove " + remove + "by id :" + id);
+    }
+
+    @Override
+    public T save(T t) {
+        int maxId = 0;
+        if (!isEmpty(nameFile)) {
+            readInFile();
+            maxId = getMaxId();
+        }
+        if (t.isNew()) {
+            maxId++;
+            t.setId(maxId);
+            repositoryInMemory.put(t.getId(), t);
+            System.out.println("create " + t + "by id :" + maxId);
+        } else {
+            repositoryInMemory.computeIfPresent(t.getId(), (id, oldUser) -> t);
+            System.out.println("update " + t + "by id :" + maxId);
+        }
+        saveInFile();
+        return t;
+    }
+
+    private void saveInFile() {
+        writeEntity(nameFile, repositoryInMemory);
+    }
+
+    private void readInFile() {
+        repositoryInMemory = readValues(nameFile, Integer.class, clazz);
+    }
+
+    private int getMaxId() {
+        return Collections.max(repositoryInMemory.keySet());
+    }
+}
