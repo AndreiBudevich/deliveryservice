3319178244ab5c524ff359caecb8c0b118d7d904
Andrei Budevich <budevichac01@gmail.com>
2022-08-02 15:57:26 +0300

    refactored Order

--- src/main/java/by/deliveryservice/model/Order.java
+++ src/main/java/by/deliveryservice/model/Order.java
@@ -1,19 +1,45 @@
 package by.deliveryservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.LongStream;
+
+import static by.deliveryservice.util.DateTimeUtil.getCurrentDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Order extends BaseEntity {
 
+    @JsonProperty
+    Client client;
+
+    @JsonProperty
     LocalDateTime dateTime;
+
+    @JsonProperty
     Shop shop;
+
+    @JsonProperty
     Long totalCost;
+
+    @JsonProperty
     String deliveryAddress;
+
+    @JsonProperty
     List <Product> products;
+
+    public Order(Client client, Shop shop) {
+        this.client = client;
+        this.dateTime = getCurrentDateTime();
+        this.shop = shop;
+        this.totalCost = products.stream().flatMapToLong(product -> LongStream.of(product.getPrice())).sum();
+        this.deliveryAddress = client.getResidentialAddress();
+    }
 }
