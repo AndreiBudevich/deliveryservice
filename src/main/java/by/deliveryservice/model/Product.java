b37a5830752d270f1a00d2ebdd173df8dd207a48
Andrei Budevich <budevichac01@gmail.com>
2022-08-02 15:57:56 +0300

    refactored Product

--- src/main/java/by/deliveryservice/model/Product.java
+++ src/main/java/by/deliveryservice/model/Product.java
@@ -1,5 +1,7 @@
 package by.deliveryservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.Set;
@@ -8,11 +10,29 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Product extends NamedEntity {
 
+    @JsonProperty
     private String description;
+
+    @JsonProperty
     private Shop shop;
+
+    @JsonProperty
     private Long price;
+
+    @JsonProperty
     private Integer discount;
+
+    @JsonProperty
     Set<Category> categories;
+
+    public Product(String name, String description, Shop shop, Long price, Integer discount) {
+        super(null, name);
+        this.description = description;
+        this.shop = shop;
+        this.price = price;
+        this.discount = discount;
+    }
 }
