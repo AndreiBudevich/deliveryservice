8f283d0a9cf04fba43b7afd327affd4010082eb0
Andrei Budevich <budevichac01@gmail.com>
2022-08-02 15:58:27 +0300

    add EntityUtil

---
+++ src/main/java/by/deliveryservice/util/EntityUtil.java
@@ -0,0 +1,65 @@
+package by.deliveryservice.util;
+
+import by.deliveryservice.model.*;
+import by.deliveryservice.repository.Repository;
+import by.deliveryservice.repository.infile.InFileClientRepository;
+import by.deliveryservice.repository.infile.InFileShopRepository;
+import lombok.experimental.UtilityClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static by.deliveryservice.util.DateTimeUtil.getDateOfBirth;
+
+@UtilityClass
+public class EntityUtil {
+
+    private static final String PATTERN_CLIENT = "*surname; name; middleName; residentialAddress; dateOfBirth*";
+    private static final String PATTERN_CATEGORY = "*name;*";
+    private static final String PATTERN_SHOP = "*name; address; description; contact*";
+    private static final String PATTERN_PRODUCT = "*name; description; id shop; price; discount*";
+    private static final String PATTERN_ORDER = "*id client; id shop*";
+
+    Map<String, String> patterns = new HashMap<>();
+
+    static {
+        patterns.put("client", PATTERN_CLIENT);
+        patterns.put("category", PATTERN_CATEGORY);
+        patterns.put("shop", PATTERN_SHOP);
+        patterns.put("order", PATTERN_ORDER);
+        patterns.put("product", PATTERN_PRODUCT);
+    }
+
+    public static BaseEntity creatEntityFromString(String stringNameEntity, String stringEntity) {
+        try {
+            String[] split = stringEntity.split("; ");
+            switch (stringNameEntity) {
+                case ("client"):
+                    return new Client(split[1], split[0], split[2], split[3], getDateOfBirth(split[4]));
+                case ("category"):
+                    return new Category(split[0]);
+                case ("order"):
+                    return new Order(getClient(split[0]), getShop(split[0]));
+                case ("product"):
+                    return new Product(split[0], split[1], getShop(split[2]), Long.parseLong(split[3]), Integer.parseInt(split[4]));
+                case ("shop"):
+                    return new Shop(split[0], split[1], split[2], split[3]);
+            }
+        } catch (Exception e) {
+            System.out.println("Не верно введены поля " + stringNameEntity + " по шаблону \n" +
+                    patterns.get(stringNameEntity));
+        }
+        return null;
+    }
+
+    private static Shop getShop(String id) {
+        Repository<Shop> shopRepository = new InFileShopRepository();
+        return shopRepository.get(Integer.parseInt(id)).orElse(null);
+    }
+
+    private static Client getClient(String id) {
+        Repository<Client> clientRepository = new InFileClientRepository();
+        return clientRepository.get(Integer.parseInt(id)).orElse(null);
+    }
+}
+
