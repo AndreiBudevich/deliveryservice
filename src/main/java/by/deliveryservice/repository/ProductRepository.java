249e49ebf57689cf1bf3b36cadd46f9e117d96cf
Andrei Budevich <budevichac01@gmail.com>
2022-08-03 16:34:25 +0300

    add #addCategories #findByAttributes

--- src/main/java/by/deliveryservice/repository/ProductRepository.java
+++ src/main/java/by/deliveryservice/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package by.deliveryservice.repository;
 
+import by.deliveryservice.model.Category;
 import by.deliveryservice.model.Product;
 
 import java.util.List;
 
 public interface ProductRepository extends Repository<Product> {
+
+    void addCategories(Integer id, Category... categories);
+
+    List<Product> findByAttributes(String... attributes);
+
     List<Product> getSortPrice();
 }
