be42b7eb237311802e57514d7eb3564e2dc1bccc
Andrei Budevich <budevichac01@gmail.com>
2022-08-03 16:28:23 +0300

    override #toString Shop

--- src/main/java/by/deliveryservice/model/Shop.java
+++ src/main/java/by/deliveryservice/model/Shop.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -23,12 +24,24 @@ public class Shop extends NamedEntity {
     private String contact;
 
     @JsonProperty
-    private List <Product> products;
+    private List <Product> products = new ArrayList<>();
 
     public Shop(String name,String address, String description, String contact) {
         super(null, name);
         this.address = address;
         this.description = description;
         this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Shop " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", description='" + description + '\'' +
+                ", contact='" + contact + '\'' +
+                ", products=" + products +
+                '}';
     }
 }
