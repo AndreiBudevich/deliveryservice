6cc0b8faabf11c11e6adbc0d5c01a5205d88d57b
Andrei Budevich <budevichac01@gmail.com>
2022-08-03 19:41:34 +0300

    impl #findByAttributes

--- src/main/java/by/deliveryservice/repository/infile/InFileProductRepository.java
+++ src/main/java/by/deliveryservice/repository/infile/InFileProductRepository.java
@@ -4,8 +4,10 @@ import by.deliveryservice.model.Category;
 import by.deliveryservice.model.Product;
 import by.deliveryservice.repository.ProductRepository;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static by.deliveryservice.util.EntityUtil.*;
 
 public class InFileProductRepository extends InFileRepository<Product> implements ProductRepository {
     public InFileProductRepository() {
@@ -31,6 +33,26 @@ public class InFileProductRepository extends InFileRepository<Product> implement
 
     @Override
     public List<Product> findByAttributes(String... attributes) {
-        return null;
+        if (attributes.length < 5) {
+            System.out.println("Не верно заданы атрибуты");
+        }
+        readInFile();
+        return repositoryInMemory.values().stream()
+                .filter(product -> containsName(product.getName(), attributes[0]))
+                .filter(product -> containsName(product.getDescription(), attributes[1]))
+                .filter(product -> containsName(product.getShop().getId().toString(), attributes[2]))
+                .filter(product -> containsName(product.getPrice().toString(), attributes[3]))
+                .filter(product -> containsName(product.getDiscount().toString(), attributes[4]))
+                .filter(product -> containsCategories(product.getCategories(), getSplit(attributes[5], ", ")))
+                .collect(Collectors.toList());
+    }
+
+    private boolean containsCategories(Set<Category> expectedCategories, String[] actualStringIdsCategories) {
+        return actualStringIdsCategories[0].equals("*") || !Collections.disjoint(expectedCategories,
+                Arrays.asList(Objects.requireNonNull(getEntitiesByIdsArray(Category.class, actualStringIdsCategories))));
+    }
+
+    private boolean containsName(String actualString, String expectedString) {
+        return expectedString.equals("*") || contains(actualString, expectedString);
     }
 }
