fa243d207d2080e52b1398bd96364fa6e24f4bf3
Andrei Budevich <budevichac01@gmail.com>
2022-08-03 21:39:43 +0300

    add StringUtil

---
+++ src/main/java/by/deliveryservice/util/StringUtil.java
@@ -0,0 +1,20 @@
+package by.deliveryservice.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Locale;
+
+@UtilityClass
+public class StringUtil {
+    public static String[] getSplit(String stringEntity, String regex) {
+        return stringEntity.split(regex);
+    }
+
+    public static boolean contains(String actualString, String expectedString) {
+        return actualString.toLowerCase(Locale.ROOT).contains(expectedString.toLowerCase(Locale.ROOT));
+    }
+
+    public static boolean equals(String actualString, String expectedString) {
+        return actualString.toLowerCase(Locale.ROOT).equals(expectedString.toLowerCase(Locale.ROOT));
+    }
+}
