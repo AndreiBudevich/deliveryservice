23bd0397225daf218bfaa5547dd0194c25a38c5a
Andrei Budevich <budevichac01@gmail.com>
2022-08-04 21:08:31 +0300

    refactored #print

--- src/main/java/by/deliveryservice/view/EntityPrint.java
+++ src/main/java/by/deliveryservice/view/EntityPrint.java
@@ -1,15 +1,18 @@
 package by.deliveryservice.view;
 
 import java.util.List;
+import java.util.Map;
 
 public class EntityPrint {
 
     public static void print(Object object) {
         if (object instanceof List) {
-            List<Object> list = (List<Object>) object;
-            for (Object o : list) {
-                System.out.println(o);
-            }
+            List<Object> objects = (List<Object>) object;
+            objects.forEach(System.out::println);
+        }
+        if (object instanceof Map) {
+            Map<Object, Object> objects = (Map<Object, Object>) object;
+            objects.entrySet().forEach(System.out::println);
         }
     }
 }
