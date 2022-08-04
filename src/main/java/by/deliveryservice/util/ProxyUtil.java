20c9b26ed914a9f53f973f47791e5ca96bb379bb
Andrei Budevich <budevichac01@gmail.com>
2022-08-04 18:57:03 +0300

    remove #getCurrentMethod

--- src/main/java/by/deliveryservice/util/ProxyUtil.java
+++ src/main/java/by/deliveryservice/util/ProxyUtil.java
@@ -4,11 +4,12 @@ import lombok.experimental.UtilityClass;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
-import static by.deliveryservice.util.MethodUtil.getCurrentMethod;
+import java.util.Arrays;
+import java.util.Locale;
 
 @UtilityClass
 public class ProxyUtil {
+
     public static Object getInstance(Class <?> clazz, String nameMethod, Object... args) {
         try {
             Method currentMethod = getCurrentMethod(clazz, nameMethod);
@@ -17,5 +18,14 @@ public class ProxyUtil {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static Method getCurrentMethod(Class<?> clazz, String nameMethod) {
+        return Arrays.stream(clazz.getMethods()).filter(m -> compareMethodName(nameMethod, m)).findFirst().orElse(null);
+    }
+
+    private static boolean compareMethodName(String expectedNameMethod, Method method) {
+        String actualMethodName = method.getName().toLowerCase(Locale.ROOT);
+        return actualMethodName.contains(expectedNameMethod.toLowerCase(Locale.ROOT));
     }
 }
