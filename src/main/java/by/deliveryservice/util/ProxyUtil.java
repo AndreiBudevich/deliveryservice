6aebefd9d399dcbb4de90b0acf03ec890832f48f
Andrei Budevich <budevichac01@gmail.com>
2022-08-04 21:21:11 +0300

    delete compareMethodName

--- src/main/java/by/deliveryservice/util/ProxyUtil.java
+++ src/main/java/by/deliveryservice/util/ProxyUtil.java
@@ -5,7 +5,6 @@ import lombok.experimental.UtilityClass;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Locale;
 
 @UtilityClass
 public class ProxyUtil {
@@ -13,6 +12,7 @@ public class ProxyUtil {
     public static Object getInstance(Class <?> clazz, String nameMethod, Object... args) {
         try {
             Method currentMethod = getCurrentMethod(clazz, nameMethod);
+            assert currentMethod != null;
             return currentMethod.invoke(clazz.newInstance(), args);
         } catch (InstantiationException | InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
@@ -21,11 +21,6 @@ public class ProxyUtil {
     }
 
     public static Method getCurrentMethod(Class<?> clazz, String nameMethod) {
-        return Arrays.stream(clazz.getMethods()).filter(m -> compareMethodName(nameMethod, m)).findFirst().orElse(null);
-    }
-
-    private static boolean compareMethodName(String expectedNameMethod, Method method) {
-        String actualMethodName = method.getName().toLowerCase(Locale.ROOT);
-        return actualMethodName.contains(expectedNameMethod.toLowerCase(Locale.ROOT));
+        return Arrays.stream(clazz.getMethods()).filter(m -> m.getName().equalsIgnoreCase(nameMethod)).findFirst().orElse(null);
     }
 }
