6acf10a02f10fb2cc81ecf37dac9afb0a4676a12
Andrei Budevich <budevichac01@gmail.com>
2022-08-04 16:53:07 +0300

    add #deleteProducts #addProducts

--- src/main/java/by/deliveryservice/repository/OrderRepository.java
+++ src/main/java/by/deliveryservice/repository/OrderRepository.java
@@ -1,6 +1,9 @@
 package by.deliveryservice.repository;
 
 import by.deliveryservice.model.Order;
+import by.deliveryservice.model.Product;
 
 public interface OrderRepository extends Repository<Order> {
+    void addProducts(Integer id, Product... products);
+    void deleteProducts(Integer id, Product... products);
 }
--- src/main/java/by/deliveryservice/repository/infile/InFileOrderRepository.java
+++ src/main/java/by/deliveryservice/repository/infile/InFileOrderRepository.java
@@ -1,7 +1,10 @@
 package by.deliveryservice.repository.infile;
 
 import by.deliveryservice.model.Order;
+import by.deliveryservice.model.Product;
 import by.deliveryservice.repository.OrderRepository;
+
+import java.util.Arrays;
 
 public class InFileOrderRepository extends InFileRepository<Order> implements OrderRepository {
 
@@ -13,5 +16,29 @@ public class InFileOrderRepository extends InFileRepository<Order> implements Or
     protected void updateEntity(Order order, Order orderOld) {
         order.setDateTime(orderOld.getDateTime());
         repositoryInMemory.computeIfPresent(order.getId(), (id, oldClient) -> order);
+    }
+
+    @Override
+    public void addProducts(Integer id, Product... products) {
+        operationsProducts(id, "add", products);
+    }
+
+    @Override
+    public void deleteProducts(Integer id, Product... products) {
+        operationsProducts(id, "delete", products);
+    }
+
+    private void operationsProducts(Integer id, String operation, Product... products) {
+        readInFile();
+        Order order = repositoryInMemory.get(id);
+        Arrays.asList(products).forEach(product -> {
+            if (operation.equals("add")) {
+                order.getProducts().add(product);
+            } else {
+                order.getProducts().remove(product);
+            }
+        });
+        saveInFile();
+        System.out.println(order);
     }
 }
