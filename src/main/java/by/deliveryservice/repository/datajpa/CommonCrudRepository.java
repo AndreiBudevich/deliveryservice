c1aed94a0749d3c5773643110778bc705b90e8ba
Andrei Budevich <budevichac01@gmail.com>
2022-08-13 12:57:16 +0300

    add CommonCrudRepository

---
+++ src/main/java/by/deliveryservice/repository/datajpa/CommonCrudRepository.java
@@ -0,0 +1,11 @@
+package by.deliveryservice.repository.datajpa;
+
+import by.deliveryservice.model.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@NoRepositoryBean
+public interface CommonCrudRepository<T extends BaseEntity> extends JpaRepository<T, Integer> {
+}
