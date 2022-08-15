c6ff83b0edca71f25e7800ace60c35d777a3c947
Andrei Budevich <budevichac01@gmail.com>
2022-08-15 14:32:23 +0300

    add AbstractCategoryController

---
+++ src/main/java/by/deliveryservice/web/category/AbstractCategoryController.java
@@ -0,0 +1,45 @@
+package by.deliveryservice.web.category;
+
+import by.deliveryservice.model.Category;
+import by.deliveryservice.repository.datajpa.DataJpaCategoryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static by.deliveryservice.util.validation.ValidationUtil.assureIdConsistent;
+import static by.deliveryservice.util.validation.ValidationUtil.checkNew;
+
+@Slf4j
+public abstract class AbstractCategoryController {
+
+    @Autowired
+    private DataJpaCategoryRepository categoryRepository;
+
+    public Category get(int id) {
+        log.info("get category {}", id);
+        return categoryRepository.get(id).orElse(null);
+    }
+
+    public void delete(int id) {
+        log.info("delete category {}", id);
+        categoryRepository.delete(id);
+    }
+
+    public List<Category> getAll() {
+        log.info("getAll for category");
+        return categoryRepository.getAll();
+    }
+
+    public Category create(Category category) {
+        checkNew(category);
+        log.info("create {}", category);
+        return categoryRepository.save(category);
+    }
+
+    public void update(Category category, int id) {
+        assureIdConsistent(category, id);
+        log.info("update {}", category);
+        categoryRepository.save(category);
+    }
+}
