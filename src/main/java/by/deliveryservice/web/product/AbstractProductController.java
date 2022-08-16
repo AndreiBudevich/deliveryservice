9d9627a2acb514e749577b70a8ef63a9090a4ac5
Andrei Budevich <budevichac01@gmail.com>
2022-08-16 18:20:42 +0300

    add AbstractProductController

---
+++ src/main/java/by/deliveryservice/web/product/AbstractProductController.java
@@ -0,0 +1,50 @@
+package by.deliveryservice.web.product;
+
+import by.deliveryservice.model.Product;
+import by.deliveryservice.repository.datajpa.DataJpaProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static by.deliveryservice.util.validation.ValidationUtil.assureIdConsistent;
+import static by.deliveryservice.util.validation.ValidationUtil.checkNew;
+
+@Slf4j
+public abstract class AbstractProductController {
+
+    @Autowired
+    private DataJpaProductRepository productRepository;
+
+    public Product get(int id) {
+        log.info("get product {}", id);
+        return productRepository.get(id).orElse(null);
+    }
+
+    public void delete(int id) {
+        log.info("delete product {}", id);
+        productRepository.delete(id);
+    }
+
+    public List<Product> getAll() {
+        log.info("getAll for product");
+        return productRepository.getAll();
+    }
+
+    public List<Product> getAllProductsByShopId(int shopId) {
+        log.info("getAll for product by shopId {}", shopId);
+        return productRepository.getAllProductsByShopId(shopId);
+    }
+
+    public Product create(Product product) {
+        checkNew(product);
+        log.info("create {}", product);
+        return productRepository.save(product);
+    }
+
+    public void update(Product product, int id) {
+        assureIdConsistent(product, id);
+        log.info("update {}", product);
+        productRepository.save(product);
+    }
+}
