6e772c96e0960f1242345c29e776f69b51ee7968
Andrei Budevich <budevichac01@gmail.com>
2022-08-22 17:58:35 +0300

    add ValidationUtil

--- src/main/java/by/deliveryservice/util/validation/ValidationUtil.java
+++ src/main/java/by/deliveryservice/util/validation/ValidationUtil.java
@@ -2,7 +2,13 @@ package by.deliveryservice.util.validation;
 
 import by.deliveryservice.HasId;
 import by.deliveryservice.error.IllegalRequestDataException;
+import by.deliveryservice.error.ErrorType;
 import lombok.experimental.UtilityClass;
+import org.slf4j.Logger;
+import org.springframework.core.NestedExceptionUtils;
+import org.springframework.lang.NonNull;
+
+import javax.servlet.http.HttpServletRequest;
 
 @UtilityClass
 public class ValidationUtil {
@@ -19,5 +25,25 @@ public class ValidationUtil {
         } else if (bean.id() != id) {
             throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must has id=" + id);
         }
+    }
+
+    @NonNull
+    public static Throwable getRootCause(@NonNull Throwable t) {
+        Throwable rootCause = NestedExceptionUtils.getRootCause(t);
+        return rootCause != null ? rootCause : t;
+    }
+
+    public static String getMessage(Throwable e) {
+        return e.getLocalizedMessage() != null ? e.getLocalizedMessage() : e.getClass().getName();
+    }
+
+    public static Throwable logAndGetRootCause(Logger log, HttpServletRequest req, Exception e, boolean logStackTrace, ErrorType errorType) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        if (logStackTrace) {
+            log.error("{} at request {}", errorType, req.getRequestURL(), rootCause);
+        } else {
+            log.warn("{} at request  {}: {}", errorType, req.getRequestURL(), rootCause);
+        }
+        return rootCause;
     }
 }
