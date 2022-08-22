d184930a5982360a67da531ba706b196dd7ef533
Andrei Budevich <budevichac01@gmail.com>
2022-08-22 17:49:33 +0300

    add category.js

---
+++ src/main/webapp/resources/js/category.js
@@ -0,0 +1,54 @@
+function makeEditableCategory(datatableOpts) {
+    $("#datatableCategory").DataTable(
+        $.extend(true, datatableOpts,
+            {
+                "ajax": {
+                    "url": "api/categories",
+                    "dataSrc": ""
+                },
+                "bFilter": false,
+                "bSort": false,
+                "bInfo": false,
+                "paging": false,
+            }
+        ));
+    form = $('#detailsForm');
+    ajaxError ();
+    ajaxSetup()
+}
+
+window.addEventListener("load", function () {
+    makeEditableCategory({
+        "columns": [
+            {
+                "data": "name"
+            },
+            {
+                "data": "enabled",
+                "render": function (data, type, row) {
+                    if (type === "display") {
+                        return "<input type='checkbox' " + ("checked") + " onclick='enable($(this));'/>";
+                    }
+                    return data;
+                }
+            }
+        ],
+        "order": [
+            [
+                0,
+                "asc"
+            ]
+        ],
+        "createdRow": function (row, data) {
+            if (!data.enabled) {
+                $(row).attr("data-category-enabled", true);
+            }
+        }
+    });
+});
+
+function enable(checkbox) {
+    let enabled = checkbox.is(":checked");
+    checkbox.closest("tr").attr("data-category-enabled", !!enabled);
+    successNoty(enabled ? "common.enabled" : "common.disabled");
+}
