75ddf00c0d0951602287e496d990e8060369627e
Andrei Budevich <budevichac01@gmail.com>
2022-08-23 22:51:30 +0300

    add ClientUIController

---
+++ src/main/java/by/deliveryservice/web/client/ClientUIController.java
@@ -0,0 +1,41 @@
+package by.deliveryservice.web.client;
+
+import by.deliveryservice.model.Client;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/clients", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ClientUIController extends AbstractClientController {
+    @GetMapping()
+    @Override
+    public List<Client> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public Client get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void createOrUpdate(Client client) {
+        if (client.isNew()) {
+            super.create(client);
+        } else {
+            super.update(client, client.getId());
+        }
+    }
+}
