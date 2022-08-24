55149d549ddb9997ec3bafd830be94f70ef4b7b0
Andrei Budevich <budevichac01@gmail.com>
2022-08-24 22:20:45 +0300

    add orders.js

---
+++ src/main/webapp/resources/js/orders.js
@@ -0,0 +1,66 @@
+const orderAjaxUrl = "api/orders/";
+
+const ctx = {
+    ajaxUrl: orderAjaxUrl,
+    updateTable: function () {
+        $.get(orderAjaxUrl, updateTableByData);
+    }
+}
+
+$(function () {
+    makeEditable({
+        "columns": [
+            {
+                "data": "client",
+                "render": function (data, type) {
+                    if (type === "display") {
+                        return "<a href='client_orders?clientId=" + data.id + "'>" + data.surname + " "
+                            + data.name.substr(0, 1) + ". " +
+                            data.middleName.substr(0, 1) + "." + "</a>";
+                    }
+                    return data;
+                }
+            },
+            {
+                "data": "registered",
+                "render": function (date, type, row) {
+                    if (type === "display") {
+                        return date.substr(0, 16).replace('T', ' ');
+                    }
+                    return date;
+                }
+            },
+            {
+                "data": "deliveryAddress"
+            },
+            {
+                "data": "totalCost"
+            },
+            {
+                "data": "shipped",
+                "render": function (data, type, row) {
+                    if (type === "display") {
+                        return "<input type='checkbox' " + (data ? "checked" : "") + " onclick='enable($(this)," + row.id + ");'/>";
+                    }
+                    return data;
+                }
+            },
+            {
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderEditBtn
+            },
+            {
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderDeleteBtn
+            }
+        ],
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ],
+    });
+});
