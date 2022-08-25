bf7dd1bada3c4e3201f3ea5ed5991df0e2ce1de9
Andrei Budevich <budevichac01@gmail.com>
2022-08-25 20:57:09 +0300

    add #findByIdWithClient

--- src/main/java/by/deliveryservice/repository/datajpa/OrderCrudRepository.java
+++ src/main/java/by/deliveryservice/repository/datajpa/OrderCrudRepository.java
@@ -4,6 +4,7 @@ import by.deliveryservice.model.Order;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderCrudRepository extends CommonCrudRepository<Order> {
 
@@ -12,4 +13,7 @@ public interface OrderCrudRepository extends CommonCrudRepository<Order> {
 
     @Query("SELECT o FROM Order o JOIN FETCH o.client WHERE o.client.id = ?1 ORDER BY o.registered ASC")
     List<Order> getAllByClientId(int id);
+
+    @Query("SELECT o FROM Order o JOIN FETCH o.client WHERE o.id = ?1")
+    Optional<Order> findByIdWithClient(int id);
 }
