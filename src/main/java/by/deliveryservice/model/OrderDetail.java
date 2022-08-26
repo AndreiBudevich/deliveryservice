635d6092522af1952fb5bc7cd69b5fe92023f6e4
Andrei Budevich <budevichac01@gmail.com>
2022-08-26 20:27:16 +0300

    add OrderDetail

---
+++ src/main/java/by/deliveryservice/model/OrderDetail.java
@@ -0,0 +1,38 @@
+package by.deliveryservice.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "order_details")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OrderDetail extends BaseEntity {
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "price", nullable = false)
+    private Long price;
+
+    @Column(name = "quanity", nullable = false)
+    private int quanity;
+
+    @Column(name = "amount", nullable = false)
+    private double amount;
+}
+
