f6678097a703719d4066090261887e25156c868b
Andrei Budevich <budevichac01@gmail.com>
2022-08-29 20:27:27 +0300

    add #get, #delete, #save

--- src/main/java/by/deliveryservice/repository/OrderDetailRepository.java
+++ src/main/java/by/deliveryservice/repository/OrderDetailRepository.java
@@ -3,8 +3,15 @@ package by.deliveryservice.repository;
 import by.deliveryservice.model.OrderDetail;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderDetailRepository {
 
-    List<OrderDetail> getByOrderId(int id);
+    List<OrderDetail> getAllByOrderId(int id);
+
+    Optional<OrderDetail> get(int id);
+
+    void delete(int id);
+
+    OrderDetail save(OrderDetail orderDetail, int orderId, int productId);
 }
