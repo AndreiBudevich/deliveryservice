37fa46e04e30f58db0bf3bc620f038eaeb052589
Andrei Budevich <budevichac01@gmail.com>
2022-08-29 20:28:31 +0300

    add #get, #delete, #save

--- src/main/java/by/deliveryservice/repository/datajpa/DataJpaOrderDetailRepository.java
+++ src/main/java/by/deliveryservice/repository/datajpa/DataJpaOrderDetailRepository.java
@@ -1,22 +1,49 @@
 package by.deliveryservice.repository.datajpa;
 
+import by.deliveryservice.model.Order;
 import by.deliveryservice.model.OrderDetail;
+import by.deliveryservice.model.Product;
 import by.deliveryservice.repository.OrderDetailRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class DataJpaOrderDetailRepository implements OrderDetailRepository {
 
-    OrderDetailCrudRepository orderDetailCrudRepository;
+    private final OrderDetailCrudRepository orderDetailCrudRepository;
+    private final OrderCrudRepository orderCrudRepository;
+    private final ProductCrudRepository productCrudRepository;
 
-    public DataJpaOrderDetailRepository(OrderDetailCrudRepository orderDetailCrudRepository) {
+    public DataJpaOrderDetailRepository(OrderDetailCrudRepository orderDetailCrudRepository,
+                                        OrderCrudRepository orderCrudRepository, ProductCrudRepository productCrudRepository) {
         this.orderDetailCrudRepository = orderDetailCrudRepository;
+        this.orderCrudRepository = orderCrudRepository;
+        this.productCrudRepository = productCrudRepository;
     }
 
     @Override
-    public List<OrderDetail> getByOrderId(int id) {
-        return orderDetailCrudRepository.getByOrderId(id);
+    public List<OrderDetail> getAllByOrderId(int id) {
+        return orderDetailCrudRepository.getAllByOrderId(id);
+    }
+
+    @Override
+    public Optional<OrderDetail> get(int id) {
+        return orderDetailCrudRepository.get(id);
+    }
+
+    @Override
+    public void delete(int id) {
+        orderDetailCrudRepository.deleteById(id);
+    }
+
+    @Override
+    public OrderDetail save(OrderDetail orderDetail, int orderId, int productId) {
+        Order order = orderCrudRepository.getReferenceById(orderId);
+        Product product = productCrudRepository.getReferenceById(productId);
+        orderDetail.setOrder(order);
+        orderDetail.setProduct(product);
+        return orderDetailCrudRepository.save(orderDetail);
     }
 }
