73eb7a30c78302844ecef03a2654b5a54660f5d0
Andrei Budevich <budevichac01@gmail.com>
2022-08-30 17:08:03 +0300

    getAllByOrderId->getAllByOrderIdWithProduct

--- src/main/java/by/deliveryservice/repository/datajpa/OrderDetailCrudRepository.java
+++ src/main/java/by/deliveryservice/repository/datajpa/OrderDetailCrudRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface OrderDetailCrudRepository extends CommonCrudRepository<OrderDetail> {
 
     @Query("SELECT od FROM OrderDetail od JOIN FETCH od.product WHERE od.order.id =?1 ORDER BY od.product.name ASC")
-    List<OrderDetail> getAllByOrderId(int id);
+    List<OrderDetail> getAllByOrderIdWithProduct(int orderId);
 
     @Query("SELECT od FROM OrderDetail od JOIN FETCH od.product WHERE od.id =?1")
     Optional<OrderDetail> get(int id);
