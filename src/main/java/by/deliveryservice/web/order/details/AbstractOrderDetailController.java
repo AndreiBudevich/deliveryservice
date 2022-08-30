4f10380ec94ecb864f714057665075d996fc7c20
Andrei Budevich <budevichac01@gmail.com>
2022-08-30 16:58:05 +0300

    add AbstractOrderDetailController

--- src/main/java/by/deliveryservice/web/order/details/AbstractOrderDetailController.java
+++ src/main/java/by/deliveryservice/web/order/details/AbstractOrderDetailController.java
@@ -2,12 +2,14 @@ package by.deliveryservice.web.order.details;
 
 import by.deliveryservice.model.OrderDetail;
 import by.deliveryservice.repository.OrderDetailRepository;
+import by.deliveryservice.service.OrderDetailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 import static by.deliveryservice.util.validation.ValidationUtil.assureIdConsistent;
+import static by.deliveryservice.util.validation.ValidationUtil.checkNew;
 
 @Slf4j
 public class AbstractOrderDetailController {
@@ -15,9 +17,12 @@ public class AbstractOrderDetailController {
     @Autowired
     private OrderDetailRepository orderDetailRepository;
 
-    List<OrderDetail> getAllByOrderId(int clientId, int orderId) {
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    List<OrderDetail> getAllByOrderIdWithProduct(int clientId, int orderId) {
         log.info("get all order details by order {} by client id {}", orderId, clientId);
-        return orderDetailRepository.getAllByOrderId(orderId);
+        return orderDetailRepository.getAllByOrderIdWithProduct(orderId);
     }
 
     public OrderDetail get(int clientId, int orderId, int id) {
@@ -29,6 +34,12 @@ public class AbstractOrderDetailController {
         assureIdConsistent(orderDetail, id);
         log.info("update order details {} by product {} by order {} by client id {}", orderDetail, productId, orderId, clientId);
         orderDetailRepository.save(orderDetail, orderId, productId);
+    }
+
+    public OrderDetail create(OrderDetail orderDetail, int clientId, int orderId, int productId) {
+        checkNew(orderDetail);
+        log.info("create order details by product {} by order {} by client id {}", productId, orderId, clientId);
+        return orderDetailService.create(orderDetail, orderId, productId);
     }
 
     public void delete(int clientId, int orderId, int id) {
