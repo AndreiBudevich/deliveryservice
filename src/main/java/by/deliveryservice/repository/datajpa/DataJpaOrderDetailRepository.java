09aaaf3cfd3b0323335946ab54673daed3095643
Andrei Budevich <budevichac01@gmail.com>
2022-08-30 17:07:37 +0300

    refactored #save

--- src/main/java/by/deliveryservice/repository/datajpa/DataJpaOrderDetailRepository.java
+++ src/main/java/by/deliveryservice/repository/datajpa/DataJpaOrderDetailRepository.java
@@ -5,6 +5,7 @@ import by.deliveryservice.model.OrderDetail;
 import by.deliveryservice.model.Product;
 import by.deliveryservice.repository.OrderDetailRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,8 +25,8 @@ public class DataJpaOrderDetailRepository implements OrderDetailRepository {
     }
 
     @Override
-    public List<OrderDetail> getAllByOrderId(int id) {
-        return orderDetailCrudRepository.getAllByOrderId(id);
+    public List<OrderDetail> getAllByOrderIdWithProduct(int orderId) {
+        return orderDetailCrudRepository.getAllByOrderIdWithProduct(orderId);
     }
 
     @Override
@@ -39,11 +40,24 @@ public class DataJpaOrderDetailRepository implements OrderDetailRepository {
     }
 
     @Override
+    @Transactional
     public OrderDetail save(OrderDetail orderDetail, int orderId, int productId) {
         Order order = orderCrudRepository.getReferenceById(orderId);
-        Product product = productCrudRepository.getReferenceById(productId);
+        Product product = productCrudRepository.findById(productId).orElse(null);
+        if (orderDetail.isNew() && product != null) {
+            orderDetail.setProduct(product);
+            orderDetail.setPrice(product.getPrice());
+            orderDetail.setQuantity(1);
+        }
         orderDetail.setOrder(order);
-        orderDetail.setProduct(product);
-        return orderDetailCrudRepository.save(orderDetail);
+        return orderDetailCrudRepository.save(getWithAmountRecalculation(orderDetail));
+    }
+
+    private OrderDetail getWithAmountRecalculation(OrderDetail orderDetail) {
+        orderDetail.setAmount(orderDetail.getPrice() * orderDetail.getQuantity());
+        return orderDetail;
     }
 }
+
+
+
