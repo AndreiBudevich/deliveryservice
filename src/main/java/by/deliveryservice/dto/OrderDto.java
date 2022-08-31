6ce8986c5331be51d7a67be02293776770a1fe04
Andrei Budevich <budevichac01@gmail.com>
2022-08-31 22:29:09 +0300

    add fields registered, deliveryAddress, shipped

--- src/main/java/by/deliveryservice/dto/OrderDto.java
+++ src/main/java/by/deliveryservice/dto/OrderDto.java
@@ -1,13 +1,30 @@
 package by.deliveryservice.dto;
 
+import by.deliveryservice.model.BaseEntity;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
 @Value
-public class OrderDto {
+public class OrderDto extends BaseEntity {
+
+    LocalDateTime registered;
 
     Long totalCost;
 
-    public OrderDto(Long totalCost) {
+    String deliveryAddress;
+
+    boolean shipped;
+
+    public OrderDto(Integer id, LocalDateTime registered, Long totalCost, String deliveryAddress, boolean shipped) {
+        super(id);
+        this.registered = registered;
         this.totalCost = totalCost;
+        this.deliveryAddress = deliveryAddress;
+        this.shipped = shipped;
     }
 }
+
+
--- src/main/java/by/deliveryservice/util/OrderUtil.java
+++ src/main/java/by/deliveryservice/util/OrderUtil.java
@@ -4,10 +4,19 @@ import by.deliveryservice.dto.OrderDto;
 import by.deliveryservice.model.Order;
 import lombok.experimental.UtilityClass;
 
+import java.util.Collection;
+import java.util.List;
+
 @UtilityClass
 public class OrderUtil {
 
+    public static List<OrderDto> getDtos(Collection<Order> orders) {
+        return orders.stream()
+                .map(OrderUtil::createDto)
+                .toList();
+    }
+
     public static OrderDto createDto(Order order) {
-        return new OrderDto(order.getTotalCost());
+        return new OrderDto(order.getId(), order.getRegistered(), order.getTotalCost(), order.getDeliveryAddress(), order.isShipped());
     }
 }
