3595e8afaf16fa2228dcc50ab56c099f596f3960
Andrei Budevich <budevichac01@gmail.com>
2022-08-31 23:24:58 +0300

    refactored

--- src/main/java/by/deliveryservice/service/OrderDetailService.java
+++ src/main/java/by/deliveryservice/service/OrderDetailService.java
@@ -1,29 +1,84 @@
 package by.deliveryservice.service;
 
+import by.deliveryservice.model.Order;
 import by.deliveryservice.model.OrderDetail;
+import by.deliveryservice.model.Product;
 import by.deliveryservice.repository.OrderDetailRepository;
+import by.deliveryservice.repository.OrderRepository;
+import by.deliveryservice.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class OrderDetailService {
 
     private final OrderDetailRepository orderDetailRepository;
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
 
-    public OrderDetailService(OrderDetailRepository orderDetailRepository) {
+    public OrderDetailService(OrderDetailRepository orderDetailRepository, ProductRepository productRepository, OrderRepository orderRepository) {
         this.orderDetailRepository = orderDetailRepository;
+        this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
     }
 
-    public OrderDetail create(OrderDetail orderDetail, int orderId, int productId) {
-        OrderDetail orderDetailBD = orderDetailRepository.getAllByOrderIdWithProduct(orderId).stream()
-                .filter(od -> od.getProduct().getId() == productId)
-                .findFirst()
-                .orElse(null);
-        if (orderDetailBD == null) {
-            return orderDetailRepository.save(orderDetail, orderId, productId);
+    public List<OrderDetail> getAllByOrderIdWithProduct(int orderId) {
+        return orderDetailRepository.getAllByOrderIdWithProduct(orderId);
+    }
+
+    public OrderDetail get(int id) {
+        return orderDetailRepository.get(id).orElse(null);
+    }
+
+    public void delete(int id) {
+        orderDetailRepository.delete(id);
+    }
+
+    @Transactional
+    public void update(OrderDetail orderDetail, int orderId, int productId, int clientId) {
+        saveOrderDetailAndOrderTotalCostRecalculation (orderDetail, orderId, productId, clientId);
+    }
+
+    @Transactional
+    public void addProduct(int orderId, int productId, int clientId) {
+        OrderDetail orderDetail = orderDetailRepository.getByOrderIdByProductId(orderId, productId).orElse(null);
+        if (orderDetail == null) {
+            Product product = productRepository.get(productId).orElse(null);
+            if (product == null) {
+                throw new IllegalArgumentException();
+            }
+            orderDetail = new OrderDetail(null, null, product.getPrice(), 0, 0L);
         }
-        int quantity = orderDetailBD.getQuantity();
+
+        int quantity = orderDetail.getQuantity();
         quantity++;
-        orderDetailBD.setQuantity(quantity);
-        return orderDetailRepository.save(orderDetailBD, orderId, productId);
+        orderDetail.setQuantity(quantity);
+
+        saveOrderDetailAndOrderTotalCostRecalculation (orderDetail, orderId, productId, clientId);
+    }
+
+    private Long getActualTotalCostOrder(List<OrderDetail> orderDetails) {
+        return orderDetails.stream()
+                .mapToLong(OrderDetail::getAmount)
+                .sum();
+    }
+
+    private void orderTotalCostRecalculation(List<OrderDetail> orderDetails, int orderId, int clientId) {
+        Order order = orderRepository.get(orderId).orElse(null);
+        if (order != null) {
+            order.setTotalCost(getActualTotalCostOrder(orderDetails));
+            orderRepository.save(order, clientId);
+        }
+    }
+
+    private void saveOrderDetailAndOrderTotalCostRecalculation (OrderDetail orderDetail, int orderId, int productId, int clientId) {
+        orderDetailRepository.save(orderDetail, orderId, productId);
+        orderTotalCostRecalculation(orderDetailRepository.getAllByOrderId(orderId), orderId, clientId);
     }
 }
+
+
+
+
