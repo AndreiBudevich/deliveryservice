d19b9ee994068a600f711d50a4e64481885789e5
Andrei Budevich <budevichac01@gmail.com>
2022-08-31 22:30:52 +0300

    delete #addProducts, #deleteProduct, #setAddress

--- src/main/java/by/deliveryservice/repository/OrderRepository.java
+++ src/main/java/by/deliveryservice/repository/OrderRepository.java
@@ -1,15 +1,8 @@
 package by.deliveryservice.repository;
 
 import by.deliveryservice.model.Order;
-import by.deliveryservice.model.Product;
 
 public interface OrderRepository extends BaseRepository<Order> {
 
     Order save(Order order, int clientId);
-
-    void addProducts(Integer id, Product... products);
-
-    void deleteProducts(Integer id, Product... products);
-
-    void setAddress(Integer id, String deliveryAddress);
 }
