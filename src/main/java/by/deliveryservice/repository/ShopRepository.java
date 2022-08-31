8016aa23db5757893b649069c8d5b03a01dcd8de
Andrei Budevich <budevichac01@gmail.com>
2022-08-31 21:46:13 +0300

    delete addProducts, deleteProducts, getShopProducts

--- src/main/java/by/deliveryservice/repository/ShopRepository.java
+++ src/main/java/by/deliveryservice/repository/ShopRepository.java
@@ -1,17 +1,7 @@
 package by.deliveryservice.repository;
 
-import by.deliveryservice.model.Product;
 import by.deliveryservice.model.Shop;
 
-import java.util.Map;
-
 public interface ShopRepository extends BaseRepository<Shop> {
-
     Shop save(Shop shop);
-
-    void addProducts(Integer id, Product... products);
-
-    void deleteProducts(Integer id, Product... products);
-
-    Map<Product, Long> getShopProducts(Integer id);
 }
