04490bea02c3623f661d7481ecd7459b37b05bdd
Andrei Budevich <budevichac01@gmail.com>
2022-08-31 23:18:00 +0300

    add #getAllByOrderId, #getByOrderIdByProductId, refactored get

--- src/main/java/by/deliveryservice/repository/datajpa/crud/OrderDetailCrudRepository.java
+++ src/main/java/by/deliveryservice/repository/datajpa/crud/OrderDetailCrudRepository.java
@@ -1,4 +1,4 @@
-package by.deliveryservice.repository.datajpa;
+package by.deliveryservice.repository.datajpa.crud;
 
 import by.deliveryservice.model.OrderDetail;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +11,12 @@ public interface OrderDetailCrudRepository extends CommonCrudRepository<OrderDet
     @Query("SELECT od FROM OrderDetail od JOIN FETCH od.product WHERE od.order.id =?1 ORDER BY od.product.name ASC")
     List<OrderDetail> getAllByOrderIdWithProduct(int orderId);
 
-    @Query("SELECT od FROM OrderDetail od JOIN FETCH od.product WHERE od.id =?1")
+    @Query("SELECT od FROM OrderDetail od WHERE od.order.id =?1")
+    List<OrderDetail> getAllByOrderId(int orderId);
+
+    @Query("SELECT od FROM OrderDetail od WHERE od.id =?1")
     Optional<OrderDetail> get(int id);
+
+    @Query("SELECT od FROM OrderDetail od WHERE od.order.id =?1 AND od.product.id =?2")
+    Optional<OrderDetail> getByOrderIdByProductId(int orderId, int productId);
 }
