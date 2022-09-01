471ca30fa5ba4ccf2d1a466dd8be27b5e72a07f4
Andrei Budevich <budevichac01@gmail.com>
2022-09-01 17:32:10 +0300

    saveOrderDetails -> refactored name -> moved

--- src/main/webapp/resources/js/orderDetails.js
+++ src/main/webapp/resources/js/orderDetails.js
@@ -49,27 +49,3 @@ function renderEditRowOrderBtn(data, type, row) {
         return "<a onclick='updateRow(" + row.id + ");'><span class='fa fa-pencil'></span></a>";
     }
 }
-
-function saveOrderDetails() {
-    let idProduct;
-
-    let datatable = $('#datatable').DataTable();
-    let data = datatable.rows().data();
-    let idRow = $('#detailsForm')[0][0].value;
-    data.each(function (value, index) {
-        let idRowActual = value['id'];
-        if (idRowActual.toString()===idRow.toString()) {
-            idProduct = value['product']['id'];
-        }
-    });
-
-    $.ajax({
-        type: "POST",
-        url: ctx.ajaxUrl + idProduct,
-        data: form.serialize()
-    }).done(function () {
-        $("#editRow").modal("hide");
-        ctx.updateTable();
-        successNoty("common.saved");
-    });
-}
