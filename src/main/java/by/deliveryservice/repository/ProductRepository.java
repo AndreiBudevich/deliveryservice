d0ba946f493d8d35ba499f22fb7ae6fc15bb6b30
Andrei Budevich <budevichac01@gmail.com>
2022-09-01 22:46:16 +0300

    refactored

--- src/main/java/by/deliveryservice/repository/ProductRepository.java
+++ src/main/java/by/deliveryservice/repository/ProductRepository.java
@@ -1,19 +1,8 @@
 package by.deliveryservice.repository;
 
-import by.deliveryservice.model.Category;
 import by.deliveryservice.model.Product;
-
-import java.util.List;
 
 public interface ProductRepository extends BaseRepository<Product> {
 
-    Product save(Product product);
-
-    void addCategories(Integer id, Category... categories);
-
-    void deleteCategories(Integer id, Category... categories);
-
-    List<Product> findByAttributes(String... attributes);
-
-    List<Product> getSortPrice();
+    Product save(Product product, int shopId);
 }
