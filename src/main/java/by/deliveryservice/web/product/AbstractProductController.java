33996631cfff66e8080e3286c8b1bd296b5a09dd
Andrei Budevich <budevichac01@gmail.com>
2022-09-01 22:47:52 +0300

    add #get add #createOrUpdate

--- src/main/java/by/deliveryservice/web/product/AbstractProductController.java
+++ src/main/java/by/deliveryservice/web/product/AbstractProductController.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Objects;
 
+import static by.deliveryservice.util.ProductUtil.createDto;
 import static by.deliveryservice.util.ProductUtil.getDtos;
 import static by.deliveryservice.util.validation.ValidationUtil.assureIdConsistent;
 import static by.deliveryservice.util.validation.ValidationUtil.checkNew;
@@ -18,13 +20,13 @@ public abstract class AbstractProductController {
     @Autowired
     private DataJpaProductRepository productRepository;
 
-    public Product get(int id) {
-        log.info("get product {}", id);
-        return productRepository.get(id).orElse(null);
+    public ProductDto get(int shopId, int id) {
+        log.info("get product {} by shop {}", id, shopId);
+        return createDto(Objects.requireNonNull(productRepository.get(id).orElse(null)));
     }
 
-    public void delete(int id) {
-        log.info("delete product {}", id);
+    public void delete(int shopId, int id) {
+        log.info("delete product {} by shop {}", id, shopId);
         productRepository.delete(id);
     }
 
@@ -33,25 +35,20 @@ public abstract class AbstractProductController {
         return getDtos(productRepository.getAll());
     }
 
-    public List<Product> getAllProductsByShopId(int shopId) {
-        log.info("getAll for product by shopId {}", shopId);
-        return productRepository.getAllProductsByShopId(shopId);
-    }
-
-    public Product create(Product product) {
+    public Product create(Product product, int shopId) {
         checkNew(product);
         log.info("create {}", product);
-        return productRepository.save(product);
+        return productRepository.save(product, shopId);
     }
 
-    public void update(Product product, int id) {
+    public void update(Product product, int shopId, int id) {
         assureIdConsistent(product, id);
-        log.info("update {}", product);
-        productRepository.save(product);
+        log.info("update {} by shop {}", product, shopId);
+        productRepository.save(product, shopId);
     }
 
     List<ProductDto> getAllWithFilter(String nameContains, String descriptionContains, String shopNameContains, Long priceFrom, Long priceUpTo, Integer discountFrom,
-                                   Integer discountUpTo, String[] idsCategories) {
+                                      Integer discountUpTo, String[] idsCategories) {
         log.info("get all with filter");
         return getDtos(productRepository.getAllWithFilter(nameContains, descriptionContains, shopNameContains, priceFrom, priceUpTo,
                 discountFrom, discountUpTo, idsCategories));
--- src/main/java/by/deliveryservice/web/product/ProductUIController.java
+++ src/main/java/by/deliveryservice/web/product/ProductUIController.java
@@ -3,6 +3,7 @@ package by.deliveryservice.web.product;
 import by.deliveryservice.dto.ProductDto;
 import by.deliveryservice.model.Product;
 import com.sun.istack.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
@@ -10,31 +11,41 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/products", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/shops", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductUIController extends AbstractProductController {
 
     @Override
-    @GetMapping
+    @GetMapping("/products")
     public List<ProductDto> getAll() {
         return super.getAll();
     }
 
     @Override
-    @GetMapping("/{id}")
-    public Product get(@PathVariable int id) {
-        return super.get(id);
+    @GetMapping("/{shopId}/products/{id}")
+    public ProductDto get(@PathVariable int shopId, @PathVariable int id) {
+        return super.get(shopId, id);
     }
 
-    @GetMapping("/filter")
+    @GetMapping("/products/filter")
     public List<ProductDto> getAllWithFilter(@RequestParam @Nullable String nameContains,
-                                          @RequestParam @Nullable String descriptionContains,
-                                          @RequestParam @Nullable String shopNameContains,
-                                          @RequestParam @NotNull Long priceFrom,
-                                          @RequestParam @NotNull Long priceUpTo,
-                                          @RequestParam @NotNull Integer discountFrom,
-                                          @RequestParam @NotNull Integer discountUpTo,
-                                          @RequestParam @NotNull String categories) {
+                                             @RequestParam @Nullable String descriptionContains,
+                                             @RequestParam @Nullable String shopNameContains,
+                                             @RequestParam @NotNull Long priceFrom,
+                                             @RequestParam @NotNull Long priceUpTo,
+                                             @RequestParam @NotNull Integer discountFrom,
+                                             @RequestParam @NotNull Integer discountUpTo,
+                                             @RequestParam @NotNull String categories) {
         return super.getAllWithFilter(nameContains, descriptionContains, shopNameContains, priceFrom, priceUpTo,
                 discountFrom, discountUpTo, categories.split("_"));
+    }
+
+    @PostMapping("/{shopId}/products")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void createOrUpdate(Product product, @PathVariable int shopId) {
+        if (product.isNew()) {
+            super.create(product, shopId);
+        } else {
+            super.update(product, shopId, product.getId());
+        }
     }
 }
