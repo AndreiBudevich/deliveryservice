1c447581204f86df539ace1a122eb997d8750154
Andrei Budevich <budevichac01@gmail.com>
2022-09-01 17:40:14 +0300

    add #getAllWithProduct, #save

--- src/main/java/by/deliveryservice/repository/StorageRepository.java
+++ src/main/java/by/deliveryservice/repository/StorageRepository.java
@@ -2,5 +2,11 @@ package by.deliveryservice.repository;
 
 import by.deliveryservice.model.Storage;
 
+import java.util.List;
+
 public interface StorageRepository extends BaseRepository<Storage> {
+
+    List<Storage> getAllWithProduct(int shopId);
+
+    Storage save(Storage storage, int shopId, int productId);
 }
