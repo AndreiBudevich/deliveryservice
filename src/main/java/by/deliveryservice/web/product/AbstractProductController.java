9f1e1f3184841a2499d32b3e2e1a287d96c5aa5b
Andrei Budevich <budevichac01@gmail.com>
2022-09-02 15:47:32 +0300

    productRepository -> productService

--- src/main/java/by/deliveryservice/web/product/AbstractProductController.java
+++ src/main/java/by/deliveryservice/web/product/AbstractProductController.java
@@ -2,12 +2,11 @@ package by.deliveryservice.web.product;
 
 import by.deliveryservice.dto.ProductDto;
 import by.deliveryservice.model.Product;
-import by.deliveryservice.repository.datajpa.DataJpaProductRepository;
+import by.deliveryservice.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Objects;
 
 import static by.deliveryservice.util.ProductUtil.createDto;
 import static by.deliveryservice.util.ProductUtil.getDtos;
@@ -18,39 +17,39 @@ import static by.deliveryservice.util.validation.ValidationUtil.checkNew;
 public abstract class AbstractProductController {
 
     @Autowired
-    private DataJpaProductRepository productRepository;
+    private ProductService productService;
 
     public ProductDto get(int shopId, int id) {
         log.info("get product {} by shop {}", id, shopId);
-        return createDto(Objects.requireNonNull(productRepository.get(id).orElse(null)));
+        return createDto(productService.get(id));
     }
 
     public void delete(int shopId, int id) {
         log.info("delete product {} by shop {}", id, shopId);
-        productRepository.delete(id);
+        productService.delete(id);
     }
 
     public List<ProductDto> getAll() {
         log.info("getAll for product");
-        return getDtos(productRepository.getAll());
+        return getDtos(productService.getAll());
     }
 
     public Product create(Product product, int shopId) {
         checkNew(product);
         log.info("create {}", product);
-        return productRepository.save(product, shopId);
+        return productService.save(product, shopId);
     }
 
     public void update(Product product, int shopId, int id) {
         assureIdConsistent(product, id);
         log.info("update {} by shop {}", product, shopId);
-        productRepository.save(product, shopId);
+        productService.save(product, shopId);
     }
 
     List<ProductDto> getAllWithFilter(String nameContains, String descriptionContains, String shopNameContains, Long priceFrom, Long priceUpTo, Integer discountFrom,
                                       Integer discountUpTo, String[] idsCategories) {
         log.info("get all with filter");
-        return getDtos(productRepository.getAllWithFilter(nameContains, descriptionContains, shopNameContains, priceFrom, priceUpTo,
+        return getDtos(productService.getAllWithFilter(nameContains, descriptionContains, shopNameContains, priceFrom, priceUpTo,
                 discountFrom, discountUpTo, idsCategories));
     }
 }
