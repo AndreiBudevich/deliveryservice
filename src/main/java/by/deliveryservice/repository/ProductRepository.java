8a29ef723ef9b1ddbb852b7fbeb3c20bca893539
Andrei Budevich <budevichac01@gmail.com>
2022-09-02 18:17:37 +0300

    add #getWithCategories

--- src/main/java/by/deliveryservice/repository/ProductRepository.java
+++ src/main/java/by/deliveryservice/repository/ProductRepository.java
@@ -2,7 +2,15 @@ package by.deliveryservice.repository;
 
 import by.deliveryservice.model.Product;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends BaseRepository<Product> {
+
+    List<Product> getAllWithFilter(String nameContains, String descriptionContains, String shopNameContains, Long priceFrom, Long priceUpTo,
+                                   Integer discountFrom, Integer discountUpTo, String[] actualStringIdsCategories);
+
+    Optional<Product> getWithCategories(int id);
 
     Product save(Product product, int shopId);
 }
