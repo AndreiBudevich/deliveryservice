7034defba7765fc63d672edc30ce2ddc44b669f0
Andrei Budevich <budevichac01@gmail.com>
2022-09-03 19:24:21 +0300

    create orderProductsPlusMinus

---
+++ src/main/webapp/resources/js/orderProductsPlusMinus.js
@@ -0,0 +1,23 @@
+function plusProduct(id) {
+    $.ajax({
+        url: orderAddProduct + id,
+        type: "POST",
+    }).done(function () {
+        successNoty("product.order.added");
+        updateOrderTotalCost();
+    }).fail(function () {
+        successNoty("product.order.notAdded");
+    });
+}
+
+function minusProduct(id) {
+    $.ajax({
+        url: orderDeleteProduct + id,
+        type: "POST",
+    }).done(function () {
+        successNoty("product.order.deleted");
+        updateOrderTotalCost();
+    }).fail(function () {
+        successNoty("product.order.notDeleted");
+    });
+}
