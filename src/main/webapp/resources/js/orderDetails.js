7f6ff53b6b3530fc94b87dcd7b9fe0e52d338756
Andrei Budevich <budevichac01@gmail.com>
2022-09-03 19:23:42 +0300

    add #plusProduct #minusProduc refactored #saveWithIdProduct

--- src/main/webapp/resources/js/orderDetails.js
+++ src/main/webapp/resources/js/orderDetails.js
@@ -1,5 +1,3 @@
-const parameters = getUrlParameters();
-const orderDetailsAjaxUrl = "api/clients/" + parameters[1] + "/orders/" + parameters[2] + "/details/";
 
 const ctx = {
     ajaxUrl: orderDetailsAjaxUrl,
@@ -37,12 +35,12 @@ $(function () {
             {
                 "orderable": false,
                 "defaultContent": "",
-                "render": renderEditRowOrderBtn
+                "render": renderEditBtn
             },
             {
                 "orderable": false,
                 "defaultContent": "",
-                "render": renderDeleteBtn
+                "render": renderDeleteRowOrderBtn
             }
         ],
         "order": [
@@ -54,20 +52,95 @@ $(function () {
     });
 });
 
-function renderEditRowOrderBtn(data, type, row) {
-    if (type === "display") {
-        return "<a onclick='updateRow(" + row.id + ");'><span class='fa fa-pencil'></span></a>";
-    }
-}
-
 function renderPlusBtn(data, type, row) {
     if (type === "display") {
-        return "<a onclick='plus(" + row.id + ");'><span class='fa fa-plus'></span></a>";
+        return "<a onclick='plusWithUpdateRow(" + row.id + ");'><span class='fa fa-plus'></span></a>";
     }
 }
 
 function renderMinusBtn(data, type, row) {
     if (type === "display") {
-        return "<a onclick='minus(" + row.id + ");'><span class='fa fa-minus'></span></a>";
+        return "<a onclick='minusWithUpdateRow(" + row.id + ");'><span class='fa fa-minus'></span></a>";
     }
+}
+
+function renderDeleteRowOrderBtn(data, type, row) {
+    if (type === "display") {
+        return "<a onclick='deleteRowWithUpdateTotalCost(" + row.id + ");'><span class='fa fa-remove'></span></a>";
+    }
+}
+
+function plusWithUpdateRow(idRow) {
+    plusProduct(getIdProduct(idRow));
+}
+
+function minusWithUpdateRow(idRow) {
+    minusProduct(getIdProduct(idRow));
+}
+
+function deleteRowWithUpdateTotalCost(id) {
+    if (confirm(i18n['common.confirm'])) {
+        $.ajax({
+            url: ctx.ajaxUrl + id,
+            type: "DELETE"
+        }).done(function () {
+            ctx.updateTable();
+            updateOrderTotalCost();
+            successNoty("common.deleted");
+        });
+    }
+}
+
+function plusProduct(id) {
+    $.ajax({
+        url: orderAddProduct + id,
+        type: "POST",
+    }).done(function () {
+        successNoty("product.order.added");
+        ctx.updateTable();
+        updateOrderTotalCost();
+    }).fail(function () {
+        successNoty("product.order.notAdded");
+    });
+}
+
+function minusProduct(id) {
+    $.ajax({
+        url: orderDeleteProduct + id,
+        type: "POST",
+    }).done(function () {
+        successNoty("product.order.deleted");
+        ctx.updateTable();
+        updateOrderTotalCost();
+    }).fail(function () {
+        successNoty("product.order.notDeleted");
+    });
+}
+
+function saveWithIdProduct() {
+    let idRow = $('#detailsForm')[0][0].value;
+
+    $.ajax({
+        type: "POST",
+        url: ctx.ajaxUrl + getIdProduct(idRow),
+        data: form.serialize()
+    }).done(function () {
+        $("#editRow").modal("hide");
+        ctx.updateTable();
+        updateOrderTotalCost();
+        successNoty("common.saved");
+    });
+}
+
+function getIdProduct (idRow) {
+    let idProduct;
+    let datatable = $('#datatable').DataTable();
+    let data = datatable.rows().data();
+    data.each(function (value) {
+        let idRowActual = value['id'];
+        if (idRowActual.toString()===idRow.toString()) {
+            idProduct = value['product']['id'];
+        }
+    });
+    return idProduct;
 }
