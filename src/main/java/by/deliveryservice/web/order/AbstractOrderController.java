094fa8bfc2e1ba30fbb7ad37e2a7e5b38a061162
Andrei Budevich <budevichac01@gmail.com>
2022-09-05 15:53:04 +0300

    add #ship

--- src/main/java/by/deliveryservice/web/order/AbstractOrderController.java
+++ src/main/java/by/deliveryservice/web/order/AbstractOrderController.java
@@ -2,12 +2,11 @@ package by.deliveryservice.web.order;
 
 import by.deliveryservice.dto.OrderDto;
 import by.deliveryservice.model.Order;
-import by.deliveryservice.repository.OrderRepository;
+import by.deliveryservice.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Objects;
 
 import static by.deliveryservice.util.OrderUtil.createDto;
 import static by.deliveryservice.util.OrderUtil.getDtos;
@@ -18,37 +17,42 @@ import static by.deliveryservice.util.validation.ValidationUtil.checkNew;
 public abstract class AbstractOrderController {
 
     @Autowired
-    private OrderRepository orderRepository;
+    private OrderService orderService;
 
     public List<Order> getAll() {
         log.info("getAll for order");
-        return orderRepository.getAll();
+        return orderService.getAll();
     }
 
     public List<OrderDto> getAllByClientId(int clientId) {
         log.info("getAll for order by client id {}", clientId);
-        return getDtos(orderRepository.getAllByClientId(clientId));
+        return getDtos(orderService.getAllByClientId(clientId));
     }
 
     public OrderDto get(int id, int clientId) {
         log.info("get order {} by client id {}", id, clientId);
-        return createDto(Objects.requireNonNull(orderRepository.get(id).orElse(null)));
+        return createDto(orderService.get(id));
     }
 
     public void delete(int id, int clientId) {
         log.info("delete order {} by client id {}", id, clientId);
-        orderRepository.delete(id);
+        orderService.delete(id);
     }
 
     public Order create(Order order, int clientId) {
         checkNew(order);
         log.info("create {}  for client {}", order, clientId);
-        return orderRepository.save(order, clientId);
+        return orderService.save(order, clientId);
     }
 
     public void update(Order order, int id, int clientId) {
         assureIdConsistent(order, id);
         log.info("update {}", order);
-        orderRepository.save(order, clientId);
+        orderService.save(order, clientId);
+    }
+
+    public void ship(int id, int clientId) {
+        log.info("shipped order {} by client {}", id, clientId);
+        orderService.ship(id);
     }
 }
