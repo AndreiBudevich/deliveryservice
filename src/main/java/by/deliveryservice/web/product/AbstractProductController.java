01a828f60a992c3934e5a1183383e73a404ab9ab
Andrei Budevich <budevichac01@gmail.com>
2022-09-05 15:55:10 +0300

    delete import

--- src/main/java/by/deliveryservice/web/product/AbstractProductController.java
+++ src/main/java/by/deliveryservice/web/product/AbstractProductController.java
@@ -6,7 +6,6 @@ import by.deliveryservice.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
