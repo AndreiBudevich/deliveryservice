de958b89a1ccb6e2832c7d431e21ebaf64da0322
Andrei Budevich <budevichac01@gmail.com>
2022-09-05 15:14:35 +0300

    refactored

--- src/main/java/by/deliveryservice/service/OrderService.java
+++ src/main/java/by/deliveryservice/service/OrderService.java
@@ -1,9 +1,20 @@
 package by.deliveryservice.service;
 
-import by.deliveryservice.model.Product;
+import by.deliveryservice.model.Order;
+
+import java.util.List;
 
 public interface OrderService {
-    void addProducts(Integer id, Product... products);
 
-    void deleteProducts(Integer id, Product... products);
+    List<Order> getAll();
+
+    List<Order> getAllByClientId(int clientId);
+
+    Order get(int id);
+
+    void delete(int id);
+
+    Order save(Order order, int clientId);
+
+    void ship(int id);
 }
