249f32b14dc70f66f81c203cf93e224700fd586f
Andrei Budevich <budevichac01@gmail.com>
2022-09-06 19:12:07 +0300

    add View

---
+++ src/main/java/by/deliveryservice/web/View.java
@@ -0,0 +1,10 @@
+package by.deliveryservice.web;
+
+import javax.validation.groups.Default;
+
+public class View {
+
+    public interface Web extends Default {}
+
+    public interface Persist extends Default {}
+}
