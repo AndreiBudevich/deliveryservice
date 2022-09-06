641adabee5d0c201dbdbce0ed7d9d5596284310f
Andrei Budevich <budevichac01@gmail.com>
2022-09-06 16:56:40 +0300

    add #recalculationStorage in #ship

--- src/main/java/by/deliveryservice/service/OrderServiceImpl.java
+++ src/main/java/by/deliveryservice/service/OrderServiceImpl.java
@@ -1,7 +1,11 @@
 package by.deliveryservice.service;
 
 import by.deliveryservice.model.Order;
+import by.deliveryservice.model.OrderDetail;
+import by.deliveryservice.model.Storage;
+import by.deliveryservice.repository.OrderDetailRepository;
 import by.deliveryservice.repository.OrderRepository;
+import by.deliveryservice.repository.StorageRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +18,13 @@ import static by.deliveryservice.util.validation.ValidationUtil.isShipped;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final OrderDetailRepository orderDetailRepository;
+    private final StorageRepository storageRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository, OrderDetailRepository orderDetailRepository, StorageRepository storageRepository) {
         this.orderRepository = orderRepository;
+        this.orderDetailRepository = orderDetailRepository;
+        this.storageRepository = storageRepository;
     }
 
     @Override
@@ -52,10 +60,20 @@ public class OrderServiceImpl implements OrderService {
     public void ship(int id) {
         Order order = getById(id);
         isShipped(order);
+        List<OrderDetail> all = orderDetailRepository.getAllByOrderIdWithProduct(id);
+        all.forEach(oD -> recalculationStorage(oD.getQuantity(), oD.getProduct().getId()));
         Objects.requireNonNull(order).setShipped(true);
     }
 
     private Order getById(int id) {
         return orderRepository.get(id).orElse(null);
+    }
+
+    private void recalculationStorage(int shippedQuantity, int productId) {
+        Storage storage = storageRepository.getByProductId(productId).orElse(null);
+        assert storage != null;
+        Integer oldQuantity = storage.getQuantity();
+        Integer newQuantity = oldQuantity - shippedQuantity;
+        storage.setQuantity(newQuantity);
     }
 }
