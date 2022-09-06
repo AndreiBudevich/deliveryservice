a03fba4da8eb5a03d7afe471e7c115bc1197972e
Andrei Budevich <budevichac01@gmail.com>
2022-09-06 16:55:31 +0300

    add #getByProductId

--- src/main/java/by/deliveryservice/repository/StorageRepository.java
+++ src/main/java/by/deliveryservice/repository/StorageRepository.java
@@ -3,10 +3,13 @@ package by.deliveryservice.repository;
 import by.deliveryservice.model.Storage;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StorageRepository extends BaseRepository<Storage> {
 
     List<Storage> getAllWithProduct(int shopId);
 
     Storage save(Storage storage, int shopId, int productId);
+
+    Optional<Storage> getByProductId(int productId);
 }
