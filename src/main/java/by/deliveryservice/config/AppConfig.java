62ab26d6b82937d26e09a3433649df4fe2c16336
Andrei Budevich <budevichac01@gmail.com>
2022-09-06 17:36:40 +0300

    Config remove AppConfig

--- src/main/java/by/deliveryservice/config/AppConfig.java
+++ src/main/java/by/deliveryservice/config/AppConfig.java
@@ -5,9 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public MessageSource messageSource() {
@@ -21,5 +28,26 @@ public class AppConfig {
     @Bean
     public MessageSourceAccessor getMessageSourceAccessor(final MessageSource messageSource) {
         return new MessageSourceAccessor(messageSource);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("ru"));
+        resolver.setCookieDomain("localhost");
+        resolver.setCookieMaxAge(60 * 60);
+        return resolver;
     }
 }
