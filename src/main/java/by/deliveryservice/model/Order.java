8ffe59628389d881d5423f429ff72d4d04a15b98
Andrei Budevich <budevichac01@gmail.com>
2022-09-08 22:51:31 +0300

    add validation

--- src/main/java/by/deliveryservice/model/Order.java
+++ src/main/java/by/deliveryservice/model/Order.java
@@ -1,5 +1,8 @@
 package by.deliveryservice.model;
 
+import by.deliveryservice.util.validation.NoHtml;
+import by.deliveryservice.web.View;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,6 +12,9 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 import static by.deliveryservice.util.DateTimeUtil.getCurrentDateTime;
@@ -24,14 +30,20 @@ public class Order extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull(groups = View.Persist.class)
+    @JsonBackReference(value = "order-client")
     private Client client;
 
     @Column(name = "registered", nullable = false, columnDefinition = "timestamp default now()", updatable = false)
-    private LocalDateTime registered;
+    private LocalDateTime registered = getCurrentDateTime();
 
     @Column(name = "total_cost", nullable = false)
+    @NotNull(groups = View.Persist.class)
     private Long totalCost = 0L;
 
+    @NotBlank
+    @Size(min = 2, max = 250)
+    @NoHtml(groups = {View.Web.class})
     @Column(name = "delivery_address", nullable = false)
     private String deliveryAddress;
 
@@ -40,7 +52,12 @@ public class Order extends BaseEntity {
 
     public Order(Client client, String deliveryAddress) {
         this.client = client;
-        this.registered = getCurrentDateTime();
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public Order(Integer id, Client client, String deliveryAddress) {
+        super(id);
+        this.client = client;
         this.deliveryAddress = deliveryAddress;
     }
 
