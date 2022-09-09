f14c050ccf7b094c6084ed20dd7df730c1a2ef99
Andrei Budevich <budevichac01@gmail.com>
2022-09-09 16:58:00 +0300

    add validation, add constructor

--- src/main/java/by/deliveryservice/model/OrderDetail.java
+++ src/main/java/by/deliveryservice/model/OrderDetail.java
@@ -1,5 +1,6 @@
 package by.deliveryservice.model;
 
+import by.deliveryservice.web.View;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AccessLevel;
@@ -8,8 +9,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -25,23 +28,40 @@ public class OrderDetail extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull(groups = View.Persist.class)
     private Order order;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull(groups = View.Persist.class)
     private Product product;
 
     @Column(name = "price", nullable = false)
+    @NotNull
+    @Range(min = 1)
     private Long price;
 
+    @NotNull
+    @Range(min = 1)
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
 
+    @NotNull
+    @Range(min = 1)
     @Column(name = "amount", nullable = false)
     private Long amount;
 
     public OrderDetail(Order order, Product product, Long price, Integer quantity, Long amount) {
+        this.order = order;
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.amount = amount;
+    }
+
+    public OrderDetail(Integer id, Order order, Product product, Long price, Integer quantity, Long amount) {
+        super(id);
         this.order = order;
         this.product = product;
         this.price = price;
