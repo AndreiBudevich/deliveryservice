81e17a7353aa243eb136a9ca58703beff8ef16f4
Andrei Budevich <budevichac01@gmail.com>
2022-09-10 11:42:31 +0300

    add JsonUtil.setMapper(mapper)

--- src/main/java/by/deliveryservice/Main.java
+++ src/main/java/by/deliveryservice/Main.java
@@ -1,10 +1,26 @@
 package by.deliveryservice;
 
-import by.deliveryservice.controller.Controller;
+import by.deliveryservice.controller.ControllerFileRepository;
+import by.deliveryservice.util.json.JsonUtil;
+import by.deliveryservice.util.json.ProductSerialize;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class Main {
+
+    private static final ObjectMapper mapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+
+    static {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(new ProductSerialize());
+        mapper.registerModule(module);
+    }
+
     public static void main(String[] args) {
-        Controller.runApplication();
+        JsonUtil.setMapper(mapper);
+        ControllerFileRepository.runApplication();
     }
 }
 
