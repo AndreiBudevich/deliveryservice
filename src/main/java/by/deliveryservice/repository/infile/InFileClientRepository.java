2dd79d8be2f5c86fbcf6ee7883496ba0e5cb425d
Andrei Budevich <budevichac01@gmail.com>
2022-09-10 20:25:32 +0300

    implements ClientRepository

--- src/main/java/by/deliveryservice/repository/infile/InFileClientRepository.java
+++ src/main/java/by/deliveryservice/repository/infile/InFileClientRepository.java
@@ -1,15 +1,17 @@
 package by.deliveryservice.repository.infile;
 
 import by.deliveryservice.model.Client;
+import by.deliveryservice.repository.ClientRepository;
 
-public class InFileClientRepository extends InFileRepository<Client> {
+public class InFileClientRepository extends InFileRepository<Client> implements ClientRepository {
 
     public InFileClientRepository() {
         super("json/clients.json", Client.class);
     }
 
     @Override
-    protected void update(Client client, Client clientOld) {
+    protected void update(Client client) {
+        Client clientOld = repositoryInMemory.get(client.getId());
         client.setRegistered(clientOld.getRegistered());
         repositoryInMemory.computeIfPresent(client.getId(), (id, oldClient) -> client);
     }
