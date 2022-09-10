89460c7a25cd566a99948d9ec1549f299de98472
Andrei Budevich <budevichac01@gmail.com>
2022-09-10 19:49:55 +0300

    create EntityBuilder

---
+++ src/main/java/by/deliveryservice/util/EntityBuilder.java
@@ -0,0 +1,50 @@
+package by.deliveryservice.util;
+
+import by.deliveryservice.model.*;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static by.deliveryservice.util.DateTimeUtil.getBirthday;
+
+@Slf4j
+@UtilityClass
+public class EntityBuilder {
+
+    private static final String PATTERN_CLIENT = "*surname; name; middleName; residentialAddress; dateOfBirth*";
+    private static final String PATTERN_CATEGORY = "*name;*";
+    private static final String PATTERN_SHOP = "*name; address; description; contact*";
+    private static final String PATTERN_PRODUCT = "*name; description; id shop; price; discount*";
+    private static final String PATTERN_ORDER = "*id client*";
+
+    Map<String, String> patterns = new HashMap<>();
+
+    static {
+        patterns.put("client", PATTERN_CLIENT);
+        patterns.put("category", PATTERN_CATEGORY);
+        patterns.put("shop", PATTERN_SHOP);
+        patterns.put("order", PATTERN_ORDER);
+        patterns.put("product", PATTERN_PRODUCT);
+    }
+
+    public static BaseEntity createEntity(String nameEntity, String[] fields) {
+        try {
+            return switch (nameEntity) {
+                case ("client") -> new Client(fields[1], fields[0], fields[2], fields[3], getBirthday(fields[4]));
+                case ("category") -> new Category(fields[0]);
+                case ("order") -> new Order(null, "");
+                case ("product") -> new Product(fields[0], fields[1], null, Long.parseLong(fields[2]), Integer.parseInt(fields[3]));
+                case ("shop") -> new Shop(fields[0], fields[1], fields[2], fields[3]);
+                default -> null;
+            };
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            log.info("Не верно введены поля {} по шаблону {}", nameEntity, patterns.get(nameEntity));
+        }
+        return null;
+    }
+}
+
+
