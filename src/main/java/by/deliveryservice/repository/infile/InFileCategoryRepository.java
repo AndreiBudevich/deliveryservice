299b0847781f1b1b2091ea3a239d8bff796bb541
Andrei Budevich <budevichac01@gmail.com>
2022-09-10 20:25:10 +0300

    add implements CategoryRepository

--- src/main/java/by/deliveryservice/repository/infile/InFileCategoryRepository.java
+++ src/main/java/by/deliveryservice/repository/infile/InFileCategoryRepository.java
@@ -1,8 +1,9 @@
 package by.deliveryservice.repository.infile;
 
 import by.deliveryservice.model.Category;
+import by.deliveryservice.repository.CategoryRepository;
 
-public class InFileCategoryRepository extends InFileRepository<Category> {
+public class InFileCategoryRepository extends InFileRepository<Category> implements CategoryRepository {
     public InFileCategoryRepository() {
         super("json/categories.json", Category.class);
     }
